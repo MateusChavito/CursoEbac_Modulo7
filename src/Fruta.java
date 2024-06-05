9beddd0b5bad31be089913678bd81d5624447264
MateusChavito <mateus.chavito@hotmail.com>
2024-06-05 17:16:32 -0300

    Tarefa de classes

---
+++ src/Fruta.java
@@ -0,0 +1,39 @@
+/**
+ * @author MateusChaves
+ */
+
+public class Fruta {
+
+    private String tipo;
+
+    private String cor;
+
+    private int valor;
+
+    // não sei oq estou fazendo da minha vida estudando isso
+
+    public String getTipo() {
+        return tipo;
+    }
+
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
